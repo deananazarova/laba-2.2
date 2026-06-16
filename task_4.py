11cea01e7b753fb065e73dd5a178085643ca78da
varume <deananazarova@gmail.com>
2026-06-17 05:22:36 +0700

    Create task_4.py

---
+++ task_4.py
@@ -0,0 +1,17 @@
+def func1():
+    x = 1
+    y = 2
+    return x + y
+
+def func2(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+
+class Test:
+    def method1(selfself):
+        pass
+
+    def method2(selfself):
+        pass
